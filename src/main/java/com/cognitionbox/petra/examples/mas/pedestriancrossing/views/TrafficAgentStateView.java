4de4205f2de209128303f704ee3b77b50d363f69
Aran Hakki <aran.hakki@gmail.com>
2026-04-04 16:07:09 +0100

    refactor of sale llm based pedestrian crossing to externalize not-determinism

--- src/main/java/com/cognitionbox/petra/examples/mas/pedestriancrossing/views/TrafficAgentStateView.java
+++ src/main/java/com/cognitionbox/petra/examples/mas/pedestriancrossing/views/TrafficAgentStateView.java
@@ -6,13 +6,13 @@ import com.cognitionbox.petra.examples.trading.strategy.data.Singleton;
 public class TrafficAgentStateView {
     private final TrafficAgent agent = Singleton.get(TrafficAgent.class);
 
-    public boolean isWaiting() { return agent.isWaiting(); }
-    public boolean isThinking() { return agent.isThinking(); }
-    public boolean isDecided() { return agent.isDecided(); }
-    public boolean isSignalled() { return agent.isSignalled(); }
+    public boolean isWaiting() { return agent.waiting(); }
+    public boolean isThinking() { return agent.thinking(); }
+    public boolean isDecided() { return agent.decided(); }
+    public boolean isSignalled() { return agent.signalled(); }
 
     // State transition mutations
-    public void start() {
+    public void think() {
         if (isWaiting()) { // Pre-condition
             agent.start();
             assert (isThinking()); // Post-condition
